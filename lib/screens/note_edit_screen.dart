c8d3e71e3d8812dac397e12a03ae90a3dfbb7c8f
EinToni <51962051+EinToni@users.noreply.github.com>
2026-09-11 18:47:42 +0200

    Update dependencies and migrate to phosphoricons_flutter
    * Replaced the deprecated phosphor_flutter with phosphoricons_flutter
    * Updated Kotlin to 2.2.20
    * Updated AGP to 8.11.1

--- lib/screens/note_edit_screen.dart
+++ lib/screens/note_edit_screen.dart
@@ -1,6 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:phosphoricons_flutter/phosphoricons_flutter.dart';
 
 import '../l10n/l10n.dart';
 import '../models/exercise.dart';
